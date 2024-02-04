a7985c6b1d170424639d2a427e3971d5bcb5b457
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-02-04 00:04:16 -0500

    add webParent module with backend sub module starter code

---
+++ BazaarProject/BazaarWebParent/BazaarBackEnd/src/main/java/com/bazaar/admin/BazaarBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.bazaar.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaarBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaarBackEndApplication.class, args);
+	}
+
+}
